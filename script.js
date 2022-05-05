b9caad9ac42571db1ac15f025f4cc84ad3a179c9
ShayasTech <shayas.tech@gmail.com>
2022-05-05 10:55:17 +0300

    Added Display Features and First Operator and Operand to calculator

--- script.js
+++ script.js
@@ -1,44 +1,82 @@
 const calculator = {
     displayValue: '0',
     firstOperand: null,
-    waitingForSecondOperator: false,
     operator: null,
+    waitingForSecondOperator: false,
 };
 
 updateDisplay();
 
-let buttons = document.querySelectorAll('button');
+const buttons = document.querySelectorAll('button');
 buttons.forEach(button => {
     button.addEventListener('click', (event) => {
-        let { target } = event;
-        if (!target.matches('button')) { return; }
+        const { target } = event;
         if (target.className == 'operator') {
-            console.log("Operator", target.value);
+            handleOperation(target.value);
+            updateDisplay();
+            console.log('Operator', target.value);
             return;
         }
         if (target.className == 'dot') {
+            inputDot(target.value);
+            updateDisplay();
             console.log('Dot', target.value);
             return;
         }
         if (target.className == 'all-clear') {
-            console.log("All Clear", target.value);
+            console.log('All Clear', target.value);
+            allClear();
+            updateDisplay();
             return;
         }
-        if (target.className == "delete") {
-            console.log("Delete", target.value);
+        if (target.className == 'delete') {
+            deleteLastEntry();
+            updateDisplay();
+            console.log('Delete Last Digit', target.value);
             return;
         }
-        if (target.className == 'equals') {
-            console.log("Equals", target.value);
-            return;
-        }
-
-        console.log("Numbers", target.value);
-
+        console.log('Number', target.value);
+        inputNum(target.value);
+        updateDisplay();
     });
 });
 
 function updateDisplay() {
-    let display = document.querySelector(".current-display");
+    const display = document.querySelector('.current-display');
     display.textContent = calculator.displayValue;
+}
+
+function inputNum(num) {
+    if (calculator.waitingForSecondOperator == true) {
+        calculator.displayValue = num;
+        calculator.waitingForSecondOperator = false;
+    } else {
+        calculator.displayValue = (calculator.displayValue == '0') ? calculator.displayValue = num : calculator.displayValue += num;
+    }
+}
+
+function allClear() {
+    calculator.displayValue = '0';
+    calculator.waitingForSecondOperator = false;
+    calculator.firstOperand = null;
+    calculator.operator = null;
+}
+
+function inputDot(dot) {
+    if (!calculator.displayValue.includes('.')) {
+        calculator.displayValue += dot;
+    }
+}
+
+function deleteLastEntry() {
+    calculator.displayValue = (calculator.displayValue.length == 1) ? calculator.displayValue = '0' : calculator.displayValue.slice(0, -1);
+}
+
+function handleOperation(nextOperator) {
+    let inputValue = parseFloat(calculator.displayValue);
+    if (calculator.firstOperand == null && !isNaN(inputValue)) {
+        calculator.firstOperand = inputValue;
+    }
+    calculator.waitingForSecondOperator = true;
+    calculator.operator = nextOperator;
 }
